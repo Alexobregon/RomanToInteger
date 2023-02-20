c4d99665692f9ac750dd29dcd643cf15090d71f6
Alexander Obregon <alexander.obregon47@gmail.com>
2023-02-20 01:54:00 -0800

    added puts of Roman numeral as key and int equivalent

--- Roman-to-integer.java
+++ Roman-to-integer.java
@@ -4,6 +4,16 @@ class Solution {
     public int romanToInt(String s) {
         int result = 0;
         HashMap<Character, Integer> map = new HashMap<Character, Integer>();
+        map.put('I', 1);
+        map.put('V', 5);
+        map.put('X', 10);
+        map.put('L', 50);
+        map.put('C', 100);
+        map.put('D', 500);
+        map.put('M', 1000);
+        
+        
+        
         return 1;
     }
     }
