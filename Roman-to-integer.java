da73cac1d54637b8cdf5d6412d3c2bf1d23edf4c
Alexander Obregon <alexander.obregon47@gmail.com>
2023-02-20 01:52:44 -0800

    added HashMap and variable result

--- Roman-to-integer.java
+++ Roman-to-integer.java
@@ -1,5 +1,9 @@
+import java.util.HashMap;
+
 class Solution {
     public int romanToInt(String s) {
-
+        int result = 0;
+        HashMap<Character, Integer> map = new HashMap<Character, Integer>();
+        return 1;
     }
     }
